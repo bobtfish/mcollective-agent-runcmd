108ced38e1c566dee5fced25a27a74defba35912
Tomas Doran <bobtfish@bobtfish.net>
2013-12-25 19:12:58 +0000

    Fix harder, log stuff

--- agent/runcmd.rb
+++ agent/runcmd.rb
@@ -2,10 +2,12 @@ module MCollective
   module Agent
     class Runcmd<RPC::Agent
       action "run" do
-        pid = `ps aux | grep nginx | grep master | awk '{print $2}'`.chomp
+        pid = `ps aux | grep nginx | grep master | | grep -v grep | awk '{print $2}' | head -n 1`.chomp
         if pid =~ /^\d+$/
+          Log.info("Found PID #{pid} for nginx - sending HUP")
           reply[:status] = system("kill -HUP #{pid}")
         else
+          Log.warn("Couldn't find PID for nginx")
           reply.fail! "Could not find PID"
         end
       end
