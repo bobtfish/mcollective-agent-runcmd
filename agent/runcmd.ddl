301f2dd6c93a394a743a4bac4ea90bd02afdc055
Tomas Doran <tdoran@yelp.com>
2014-02-17 22:57:47 +0000

    Actually try to put the commands into the config

--- agent/runcmd.ddl
+++ agent/runcmd.ddl
@@ -4,14 +4,21 @@ metadata :name => "runcmd",
          :license => "Apache2",
          :version => "0.0.2",
          :url => "https://github.com/bobtfish/mcollective-runcmd",
-         :timeout => 10
+         :timeout => 600
 
 action "run", :description => "Run a specific command" do
   display :always
+
+  input :command,
+    :description => "The keyword for the command from the config file",
+    :display_as  => "Command name"
 
   output :status,
     :description => "The command exit code",
     :display_as  => "Exit code"
 
+  output :output,
+    :description => "The command stdout/stderr",
+    :display_as  => "Exit code"
 end
 
--- agent/runcmd.rb
+++ agent/runcmd.rb
@@ -2,15 +2,31 @@ module MCollective
   module Agent
     class Runcmd<RPC::Agent
       action "run" do
-        pid = `ps aux | grep nginx | grep master | grep -v grep | awk '{print $2}' | head -n 1`.chomp
-        if pid =~ /^\d+$/
-          Log.info("Found PID #{pid} for nginx - sending HUP")
-          reply[:status] = system("kill -HUP #{pid}")
-        else
-          Log.warn("Couldn't find PID for nginx")
-          reply.fail! "Could not find PID"
-        end
+        validate :command, String
+        data = run_command request[:command]
+        reply[:status] = data[:status]
+        reply[:output] = data[:output]
       end
+
+      def run_command (command)
+        cmdline = config["runcmd.#{command}"]
+        if cmdline
+          Log.info("Found cmd #{command} running: #{cmdline}")
+          code = system "#{cmdline}"
+        else
+          Log.info("Did not find #{command}")
+          code = -256
+        end
+        { :status => code, :output => '' }
+      end
+#        pid = `ps aux | grep nginx | grep master | grep -v grep | awk '{print $2}' | head -n 1`.chomp
+#        if pid =~ /^\d+$/
+#          Log.info("Found PID #{pid} for nginx - sending HUP")
+#          reply[:status] = system("kill -HUP #{pid}")
+#        else
+#          Log.warn("Couldn't find PID for nginx")
+#          reply.fail! "Could not find PID"
+#        end
 
       def config
         Config.instance.pluginconf.reject { |k,v| ! /^runcmd\./.match(k) }
